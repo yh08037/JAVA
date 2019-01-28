6fd3517894e93b07723e4090a49ec6b982b7dd29
Dohun Kim <yh08037@gmail.com>
2019-01-28 18:14:44 +0900

    Update Client.java

--- 자바 문법 연습/Client.java
+++ 자바 문법 연습/Client.java
@@ -10,29 +10,29 @@ import javax.swing.JTextField;
 
 class ClientGUI extends JFrame{
 	JTextField jf;
-	
+
 	ActionListener myListener = new ActionListener() {	//익명 내부 클래스
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// 클라이언트세어 사용될 종이컵(socket) 생성 방법
-		    try {	
+		    try {
 			    Socket client = new Socket("155.230.57.60", 8888);
-		        
+
 		        OutputStream os = client.getOutputStream();
 		        String msg = jf.getText();
 		        os.write(msg.getBytes());
 		        jf.setText("");	// jf 초기화
-			}catch(Exception ee) {
-				System.out.println("서버 데이터 전송 중 예외 발생");
-			}
+				}catch(Exception ee) {
+						System.out.println("서버 데이터 전송 중 예외 발생");
+				}
 		}
 	};
-	
+
 	// Actionlistener는 인터페이스이다!!
 	// interface A{}
 	// class B implements A{}
 	// A x = new B(); 가능!!!
-	
+
 	ClientGUI() {
 		super("채팅 Client");
 		jf = new JTextField(10);
@@ -49,13 +49,13 @@ class ClientGUI extends JFrame{
 public class Client {
 
     public static void main(String[] args) throws UnknownHostException, IOException{
-    	
+
     	new ClientGUI();
-    	
-    	
+
+
 //    	// 클라이언트세어 사용될 종이컵(socket) 생성 방법
 //        Socket client = new Socket("155.230.57.60", 8888);
-//        
+//
 //        OutputStream os = client.getOutputStream();
 //        String msg = "메세지 내용";
 //        os.write(msg.getBytes());
