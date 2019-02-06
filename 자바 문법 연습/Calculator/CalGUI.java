22919a297cbf140109a6d9aa979f5ac61b657840
Dohun Kim <yh08037@gmail.com>
2019-02-06 20:33:42 +0900

    Update Calculator

--- 자바 문법 연습/Calculator/CalGUI.java
+++ 자바 문법 연습/Calculator/CalGUI.java
@@ -9,31 +9,38 @@ class MyCalGUI extends JFrame {
 
     CalLogic logic;
     JTextField jf;
+    String str;
+
+    ActionListener myListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String result = e.getActionCommand();
+
+            switch (result) {
+                case "c":
+                    jf.setText("c");
+                    break;
+                case "=":
+                    str = jf.getText();
+                    logic = new CalLogic();
+                    jf.setText(logic.cal(str));
+                    break;
+            }
+        }
+    };
 
     MyCalGUI() {
-
-        ActionListener myListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String result = e.getActionCommand();
-
-                switch (result) {
-                    case "c":
-                        jf.setText("c");
-
-                }
-            }
-        };
-
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500, 500);
-        this.setLocation(300, 300);
 
         // UI 배치관리자
         FlowLayout layout = new FlowLayout();
         this.setLayout(layout);
 
-        JTextField jf = new JTextField(20);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLocation(300, 300);
+
+
+        jf = new JTextField(20);
         this.add(jf);
 
         JButton[] jb = new JButton[10];
@@ -41,13 +48,19 @@ class MyCalGUI extends JFrame {
         for (int i = 0; i < 10; i++) {
             jb[i] = new JButton(i+"");
             this.add(jb[i]);
+            jb[i].addActionListener(myListener);
         }
 
         JButton c = new JButton("c");
         c.setBackground(Color.RED);
         this.add(c);
-
         c.addActionListener(myListener);
+
+        JButton eq = new JButton("=");
+        eq.setBackground(Color.BLUE);
+        this.add(eq);
+        eq.addActionListener(myListener);
+
 
         this.setVisible(true);
     }
--- 자바 문법 연습/Calculator/CalLogic.java
+++ 자바 문법 연습/Calculator/CalLogic.java
@@ -1,11 +1,30 @@
-
-class MyCalLogic {
-    String str;
-
-}
+import java.util.StringTokenizer;
 
 public class CalLogic {
-    public static void main(String[] args) {
-
+    public String cal(String str) {
+        StringTokenizer stk = new StringTokenizer(str, "+-*/", true);
+        int result = 0;
+        String temp = "";
+        while (stk.hasMoreTokens()) {
+            temp = stk.nextToken();
+            switch (temp) {
+                case "+":
+                    result += Integer.parseInt(stk.nextToken());
+                    break;
+                case "-":
+                    result -= Integer.parseInt(stk.nextToken());
+                    break;
+                case "*":
+                    result *= Integer.parseInt(stk.nextToken());
+                    break;
+                case "/":
+                    result /= Integer.parseInt(stk.nextToken());
+                    break;
+                default:
+                    result = Integer.parseInt(temp);
+            }
+        }
+        return result+"";
     }
+
 }
