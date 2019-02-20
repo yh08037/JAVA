8560094266af1febecb10d85fe93e55d7d42f5b9
Dohun Kim <yh08037@gmail.com>
2019-02-20 13:50:46 +0900

    Create AverageTest.java

---
+++ POWER JAVA/Field and Method/AverageTest.java
@@ -0,0 +1,12 @@
+
+class Average {
+    double getAverage(int a, int b) {
+        return (a + b) / 2;
+    }
+}
+
+public class AverageTest {
+    public static void main(String[] args) {
+
+    }
+}
