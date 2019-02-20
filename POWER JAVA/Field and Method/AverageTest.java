33b9ab086f57d31f201e409f8a66139b7c7f8ea9
Dohun Kim <yh08037@gmail.com>
2019-02-20 14:49:28 +0900

    Update AverageTest.java

--- POWER JAVA/Field and Method/AverageTest.java
+++ POWER JAVA/Field and Method/AverageTest.java
@@ -3,10 +3,15 @@ class Average {
     double getAverage(int a, int b) {
         return (a + b) / 2;
     }
+    double getAverage(int a, int b, int c) {
+        return (a + b + c) / 3;
+    }
 }
 
 public class AverageTest {
     public static void main(String[] args) {
-
+        Average avg = new Average();
+        System.out.println(avg.getAverage(10, 20));
+        System.out.println(avg.getAverage(10, 20, 40));
     }
 }
